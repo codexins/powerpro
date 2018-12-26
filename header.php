0a9c659ee3c8559bad2880f595a45e4ed5182644
aktarzaman <aktarzaman84@gmail.com>
2018-12-26 18:32:19 +0600

    Some Fixes on Mobile header

--- header.php
+++ header.php
@@ -66,7 +66,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="header-top<?php echo ( codexin_get_option( 'cx_enable_floating_header' ) ? ' floating-header' : '' ); ?>">
 				<div class="container-fluid">
 					<div class="row align-items-center">
-						<div class="col-12 order-2 col-sm-12 order-sm-2 col-md-12 order-md-2 col-lg-4 order-lg-1">
+						<div class="col-12 order-1 col-sm-12 order-sm-2 col-md-12 order-md-2 col-lg-4 order-lg-1">
 							<div class="header-left">
 								<?php
 								if ( codexin_get_option( 'cx_enable_header_search' ) ) {
@@ -84,7 +84,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 							</div>
 						</div>
 
-						<div class="col-12 order-1 col-sm-12 order-sm-1 col-md-12 order-md-1 col-lg-4 order-lg-2">
+						<div class="col-12 order-2 col-sm-12 order-sm-1 col-md-12 order-md-1 col-lg-4 order-lg-2">
 							<!-- Logo -->
 							<div class="logo">
 								<div class="navbar-brand">
